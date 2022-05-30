6b575cf59f476f39923856ce9bc39db2b4527c34
mvn-h24 <morning.vecher.night@yandex.ru>
2022-05-30 09:32:21 +0300

    FavoriteBtn styling

--- components/FavoriteBtn.vue
+++ components/FavoriteBtn.vue
@@ -1,7 +1,53 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import heart from '@/assets/icons/heart.svg';
+import heartOutline from '@/assets/icons/heart-outline.svg';
+
+const heartUrl = `url("${heart}")`;
+const heartOutlineUrl = `url("${heartOutline}")`;
+const props = withDefaults(
+  defineProps<{
+  active:boolean
+}>(),
+{
+  active: false
+}
+)
+</script>
 
 <template>
-  <button></button>
+  <button :class="{active: props.active}">
+  </button>
 </template>
 
-<style scoped></style>
+<style scoped>
+button{
+  position: absolute;
+  width:40px;
+  height: 40px;
+  right: 25px;
+  bottom: 25px;
+
+  background-image: v-bind(heartOutlineUrl);
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: transparent;
+  
+  border:none;
+  cursor: pointer;
+  transition: 0.2s opacity ease-in, 0.5s background-image ease-in;
+}
+button:hover{
+  background-image: v-bind(heartUrl);
+  filter:grayscale(40%);
+}
+button:active{
+  filter:none;
+}
+button.active{
+  background-image: v-bind(heartUrl);
+}
+button.active:active{
+  transform: scale(1.1);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <nav-list/>
     <content-column class="column" v-scroll-after="onScrollOverGrid">
       <grid-list class="grid-list_cats">
-        <cat-img v-for="catImg in data" :key="catImg.id" :src="catImg"/>
+        <cat-img v-for="catImg in data" :key="catImg.id" :src="catImg" class="catImage">
+          <favorite-btn class="favorite-cat-key"/>
+        </cat-img>
       </grid-list>
       <show-hidden-transition>
         <loader v-if="isLoading"> loading start</loader>
@@ -32,5 +34,11 @@ const onScrollOverGrid = (event) => {
 .column{
   margin: 50px auto;
   padding:0 60px;
+}
+.catImage .favorite-cat-key{
+  opacity: 0;
+}
+.catImage:hover .favorite-cat-key{
+  opacity: 1;
 }
 </style>
